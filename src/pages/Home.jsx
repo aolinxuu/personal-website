26c1a739c28658855ad51bf3045dbf13d4fe636e
Aolin Xu <aolinzamxu@gmail.com>
2025-04-08 21:48:19 +1000

    added milk project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ const Home = () => {
         <div className="links">
           <a href="resume.pdf">Click here for my Resume</a>
           <a href="resume.pdf">Click here for my Media Kit</a>
+          <p>FYI: this website is currently under construction so some pages/links may not work.</p>
         </div>
       </div>
       <img src="memoji.jpg" alt="Aolin's memoji" className="memoji"></img>
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -7,25 +7,22 @@ const Work = () => {
       <div className="work-title">Technical Stuff</div>
       <div className="projects-container">
         <div className="project-row">
-          <div className="project-card study-timer-card">
-            <p>LockIn - Study Timer</p>
+          <div className="project-card milk-card">
+            <p>Milk Management System</p>
             <div className="tags-container">
               <div className="tag">Python</div>
-              <div className="tag">PyQt5</div>
-            </div>
-            <div className="links-container">
-              <div className="github-link">
-                <a href="https://github.com/aolin-zhang/LockIn">view on github</a>
-              </div>
-              <div className="download-link">
-                <a href="/path-to-download">click to download my app</a>
-              </div>
+              <div className="tag">HTML/CSS</div>
+              <div className="tag">JavaScript</div>
+              <div className="tag">PostgreSQL</div>
+              <div className="tag">React</div>
+              <div className="tag">Node.js</div>
             </div>
           </div>
           <div className="project-description">
             <p>
-              A simple cross-platform study timer with button controls that plays audio cues at key intervals, changes
-              colors based on progress to help you stay focused.
+              An ongoing milk management and verification system for the Royal Hospital for Women to reduce human error
+              and improve safety in neonatal feeding. Integrated barcode scanning to streamline milk tracking, enhance
+              verification accuracy, and reduce reliance on manual checks by nurses.
             </p>
           </div>
         </div>
@@ -35,17 +32,45 @@ const Work = () => {
             <div className="tags-container">
               <div className="tag">Python</div>
               <div className="tag">Machine Learning</div>
+              <div className="tag">Recommender Systems</div>
+              <div className="tag">Collaborative Filtering</div>
+              <div className="tag">Content-Based Filtering</div>
+              <div className="tag">Matrix Factorization</div>
             </div>
             <div className="links-container">
               <div className="github-link">
-                <a href="https://github.com/aolin-zhang/LockIn">view on github</a>
+                <a href="https://github.com/EricHuynh10/comp9727-PeerCoRec">view on github</a>
               </div>
             </div>
           </div>
           <div className="project-description">
             <p>
-              Description of your work at OpenAI goes here. You can include details about your role, projects, and
-              achievements.
+              A peer company recommendation system for ASX-listed firms to enhance business analysis using collaborative
+              filtering, content-based filtering, hybrid models and matrix factorization. Achieved a recall accuracy of
+              0.3 comparable to ChatGPT 3.5 through hyperparameter tuning and cross-validation.
+            </p>
+          </div>
+        </div>
+        <div className="project-row">
+          <div className="project-card study-timer-card">
+            <p>LockIn - Study Timer</p>
+            <div className="tags-container">
+              <div className="tag">Python</div>
+              <div className="tag">PyQt5</div>
+            </div>
+            <div className="links-container">
+              <div className="github-link">
+                <a href="https://github.com/aolinxuu/lock-in">view on github</a>
+              </div>
+              <div className="download-link">
+                <a href="/LockIn.dmg">click to download my app</a>
+              </div>
+            </div>
+          </div>
+          <div className="project-description">
+            <p>
+              A simple cross-platform study timer with button controls that plays audio cues at key intervals, changes
+              colors based on progress to help you stay focused.
             </p>
           </div>
         </div>
