814b173019f40ebc6b556f006b3021a14d15d7e5
Aolin Xu <aolinzamxu@gmail.com>
2025-04-08 21:16:20 +1000

    fix work styles

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -9,33 +9,44 @@ const Work = () => {
         <div className="project-row">
           <div className="project-card study-timer-card">
             <p>LockIn - Study Timer</p>
+            <div className="tags-container">
+              <div className="tag">Python</div>
+              <div className="tag">PyQt5</div>
+            </div>
+            <div className="links-container">
+              <div className="github-link">
+                <a href="https://github.com/aolin-zhang/LockIn">view on github</a>
+              </div>
+              <div className="download-link">
+                <a href="/path-to-download">click to download my app</a>
+              </div>
+            </div>
           </div>
           <div className="project-description">
             <p>
               A simple cross-platform study timer with button controls that plays audio cues at key intervals, changes
               colors based on progress to help you stay focused.
             </p>
-            <div className="tags-container">
-              <div className="tag">Python</div>
-              <div className="tag">React</div>
-              <div className="tag">JavaScript</div>
-            </div>
           </div>
         </div>
         <div className="project-row">
           <div className="project-card company-card">
             <p>Company Recommendation System</p>
+            <div className="tags-container">
+              <div className="tag">Python</div>
+              <div className="tag">Machine Learning</div>
+            </div>
+            <div className="links-container">
+              <div className="github-link">
+                <a href="https://github.com/aolin-zhang/LockIn">view on github</a>
+              </div>
+            </div>
           </div>
           <div className="project-description">
             <p>
               Description of your work at OpenAI goes here. You can include details about your role, projects, and
               achievements.
             </p>
-            <div className="tags-container">
-              <div className="tag">Python</div>
-              <div className="tag">Machine Learning</div>
-              <div className="tag">Data Analysis</div>
-            </div>
           </div>
         </div>
       </div>
